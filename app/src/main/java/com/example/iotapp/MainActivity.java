f0feeed764bb0da8225bd1005d8f930b6abafd44
Alejandro González <andresg.vergara04@gmail.com>
2024-12-25 22:17:15 -0300

    Commit 1: Aquí ya todo funciona aksjd

--- app/src/main/java/com/example/iotapp/MainActivity.java
+++ app/src/main/java/com/example/iotapp/MainActivity.java
@@ -1,147 +1,169 @@
 package com.example.iotapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.os.Bundle;
-import android.os.Handler;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
-import android.util.Log;
-
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView temperaturaTextView;
+    private Button botonSi, botonNo;
+    private ProgressBar progressBar;
 
-    private static final String THINGSPEAK_READ_URL = "https://api.thingspeak.com/channels/2788935/feeds.json?api_key=4GGFDD4U7K3IVV43&results=1";
-    private static final String THINGSPEAK_WRITE_URL = "https://api.thingspeak.com/update?api_key=2XMA8DIRYBUCRBOM";
-
-    private TextView tempAmbiente, tempPersona, pregunta, noData;
-    private Button botonSi, botonNo, botonActualizar;
-    private AsyncHttpClient client;
-    private Handler handler;
+    private static final String THINGSPEAK_API_URL = "https://api.thingspeak.com/channels/2794908/feeds.json?api_key=RKTMBAN559EUAS9B&results=2";
+    private static final String API_KEY = "CVZSD9VIVJZNS2XG";
+    private static final String CHANNEL_URL = "https://api.thingspeak.com/update";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializar AsyncHttpClient y Handler
-        client = new AsyncHttpClient();
-        handler = new Handler();
-
-        // Inicializar vistas
-        tempAmbiente = findViewById(R.id.tempAmbiente);
-        tempPersona = findViewById(R.id.tempPersona);
-        pregunta = findViewById(R.id.pregunta);
-        noData = findViewById(R.id.noData);
+        temperaturaTextView = findViewById(R.id.temperaturaTextView);
+        progressBar = findViewById(R.id.progressBar);
+        TextView confirmationTextView = findViewById(R.id.confirmationTextView);
         botonSi = findViewById(R.id.botonSi);
         botonNo = findViewById(R.id.botonNo);
-        botonActualizar = findViewById(R.id.botonActualizar);
 
-        // Configurar botón de actualización
-        botonActualizar.setOnClickListener(view -> cargarDatos());
+        // Animación inicial
+        animateViews();
 
-        // Configurar botones Sí y No
-        botonSi.setOnClickListener(view -> handleResponse(1));
-        botonNo.setOnClickListener(view -> handleResponse(0));
+        // Inicializar notificaciones
+        crearCanalDeNotificaciones();
 
-        // Inicializar estado de botones
+        // Lógica inicial
         botonSi.setEnabled(false);
         botonNo.setEnabled(false);
-        pregunta.setVisibility(View.GONE);
+        obtenerTemperaturaDeThingSpeak();
+
+        // Botón Sí
+        botonSi.setOnClickListener(v -> {
+            mostrarMensajeConfirmacion(confirmationTextView, "Alerta activada en la habitación.", "#4CAF50");
+            enviarComandoThingSpeak(1);
+        });
+
+        // Botón No
+        botonNo.setOnClickListener(v -> {
+            mostrarMensajeConfirmacion(confirmationTextView, "Acción cancelada.", "#F44336");
+            enviarComandoThingSpeak(0);
+        });
     }
 
-    private void cargarDatos() {
-        client.get(THINGSPEAK_READ_URL, new AsyncHttpResponseHandler() {
+    private void animateViews() {
+        findViewById(R.id.preguntaTextView).animate().alpha(1).setDuration(1000).setStartDelay(500).start();
+        findViewById(R.id.actionButtons).animate().alpha(1).setDuration(1000).setStartDelay(1000).start();
+    }
+
+    private void obtenerTemperaturaDeThingSpeak() {
+        progressBar.setVisibility(View.VISIBLE);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(THINGSPEAK_API_URL, new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                try {
-                    String responseData = new String(responseBody);
-                    JSONObject json = new JSONObject(responseData);
-                    JSONArray feeds = json.getJSONArray("feeds");
-
-                    if (feeds.length() > 0) {
-                        JSONObject latestData = feeds.getJSONObject(feeds.length() - 1);
-                        final String ambiente = latestData.optString("field1", "Sin datos");
-                        final String persona = latestData.optString("field2", "Sin datos");
-                        final String field3 = latestData.optString("field3", "");
-
-                        Log.d("Datos API", "Ambiente: " + ambiente + ", Persona: " + persona + ", Field3: " + field3);
-
-                        runOnUiThread(() -> {
-                            tempAmbiente.setText("Temperatura ambiente: " + ambiente + " °C");
-                            tempPersona.setText("Temperatura persona: " + persona + " °C");
-
-                            if (field3.isEmpty()) {
-                                // Activar pregunta y botones si hay datos en field2
-                                if (!persona.equals("Sin datos")) {
-                                    pregunta.setVisibility(View.VISIBLE);
-                                    pregunta.setText("¿Considera que Persona tiene fiebre?");
-                                    botonSi.setEnabled(true);
-                                    botonNo.setEnabled(true);
-                                } else {
-                                    pregunta.setVisibility(View.GONE);
-                                    botonSi.setEnabled(false);
-                                    botonNo.setEnabled(false);
-                                }
-                            } else {
-                                // Desactivar botones y ocultar pregunta si field3 ya tiene un valor
-                                pregunta.setVisibility(View.GONE);
-                                noData.setText("Último dato con comando asignado. \n    Field3 = " + field3 + "         | 0: No, 1: Sí. |\n(Sugerencia usuario: Actualizar información)\n(Sugerencia dev: Escribir nuevo feed)");
-                                noData.setVisibility(View.VISIBLE);
-                                botonSi.setEnabled(false);
-                                botonNo.setEnabled(false);
-                                Log.d("Validación", "El último dato ya tiene un valor asignado en Field3: " + field3);
-                            }
-                        });
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                progressBar.setVisibility(View.GONE);
+                Float temperatura = parseTemperature(response);
+                if (temperatura != null) {
+                    temperaturaTextView.setText(String.format("Temperatura: %.1f°C", temperatura));
+                    boolean isHighTemperature = temperatura > 30.0;
+                    if (isHighTemperature) {
+                        mostrarNotificacion("¡RIESGO DE TEMPERATURA ALTA!");
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    botonSi.setEnabled(isHighTemperature);
+                    botonNo.setEnabled(isHighTemperature);
+                } else {
+                    temperaturaTextView.setText("Error al obtener la temperatura");
                 }
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                error.printStackTrace();
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                progressBar.setVisibility(View.GONE);
+                temperaturaTextView.setText("Error al obtener la temperatura");
+                Log.e("FetchTemperatureTask", "Error en la respuesta: " + statusCode);
             }
         });
     }
 
-    private void handleResponse(int response) {
-        botonSi.setEnabled(false);
-        botonNo.setEnabled(false);
-        pregunta.setVisibility(View.GONE);
-
-        handler.postDelayed(() -> sendField3Update(response), 3000);
+    private Float parseTemperature(JSONObject jsonData) {
+        try {
+            JSONArray feeds = jsonData.getJSONArray("feeds");
+            if (feeds.length() > 0) {
+                JSONObject latestFeed = feeds.getJSONObject(0);
+                return (float) latestFeed.getDouble("field1");
+            }
+        } catch (Exception e) {
+            Log.e("FetchTemperatureTask", "Error al parsear JSON: " + e.getMessage());
+        }
+        return null;
     }
 
-    private void sendField3Update(int value) {
-        String url = THINGSPEAK_WRITE_URL + "&field3=" + value;
+    private void mostrarNotificacion(String mensaje) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "canal_id")
+                .setSmallIcon(R.drawable.baseline_crisis_alert_24)
+                .setContentTitle("Alerta de Temperatura")
+                .setContentText(mensaje)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
-        client.get(url, new AsyncHttpResponseHandler() {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void crearCanalDeNotificaciones() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel canal = new NotificationChannel("canal_id", "Canal de alertas", NotificationManager.IMPORTANCE_HIGH);
+            canal.setDescription("Canal para notificaciones de temperatura");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(canal);
+        }
+    }
+
+    private void mostrarMensajeConfirmacion(TextView confirmationTextView, String mensaje, String color) {
+        confirmationTextView.setVisibility(View.VISIBLE);
+        confirmationTextView.setText(mensaje);
+        confirmationTextView.setTextColor(Color.parseColor(color));
+        confirmationTextView.animate().alpha(1).setDuration(500).withEndAction(() -> {
+            confirmationTextView.animate().alpha(0).setDuration(1000).setStartDelay(2000).start();
+        }).start();
+    }
+
+    private void enviarComandoThingSpeak(int comando) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = CHANNEL_URL + "?api_key=" + API_KEY + "&field2=" + comando;
+
+        client.post(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Log.d("API Write", "Field3 actualizado con valor: " + value);
+                Log.i("ThingSpeak", "Comando enviado exitosamente: " + comando);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                error.printStackTrace();
+                Log.e("ThingSpeak", "Error al enviar comando: " + error.getMessage());
             }
         });
     }
